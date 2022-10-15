5ec29521921521e9667007d1aed9d2e83d7c0427
LEES0159 <31896349+LEES0159@users.noreply.github.com>
2022-10-15 12:36:12 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,32 @@
+let Moisture = 0
+basic.forever(function () {
+    Moisture = pins.analogReadPin(AnalogPin.P1)
+    if (Moisture <= 200) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            `)
+    } else {
+        if (Moisture <= 300) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                # # # # #
+                # # # # #
+                # # # # #
+                `)
+        } else {
+            basic.showLeds(`
+                # . . . #
+                . # . # .
+                . . # . .
+                . # . # .
+                # . . . #
+                `)
+        }
+        basic.pause(2500)
+    }
+})
